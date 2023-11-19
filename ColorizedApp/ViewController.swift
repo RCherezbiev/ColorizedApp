7d5af539a425a53ad2f41363215fcbfad9c3cd9a
RCherezbiev <149434791+RCherezbiev@users.noreply.github.com>
2023-11-19 09:28:39 +0300

    Created basic interface and logic of the application

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private var colorDisplayView: UIView!
+    
+    @IBOutlet private var redLabel: UILabel!
+    @IBOutlet private var greenLabel: UILabel!
+    @IBOutlet private var blueLabel: UILabel!
+    
+    @IBOutlet private var redSlider: UISlider!
+    @IBOutlet private var greenSlider: UISlider!
+    @IBOutlet private var blueSlider: UISlider!
+    
+    var redColor = 0.0
+    var greenColor = 0.0
+    var blueColor = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
     }
-
-
+    
+    
+    @IBAction func redSliderValueChanged() {
+        redLabel.text = "Red: \(round(redSlider.value * 100) / 100)"
+        redColor = CGFloat(redSlider.value)
+        updateColorView()
+    }
+    
+    @IBAction func greenSliderValueChanged() {
+        greenLabel.text = "Green: \(round(greenSlider.value * 100) / 100)"
+        greenColor = CGFloat(greenSlider.value)
+        updateColorView()
+    }
+    
+    @IBAction func blueSliderValueChanged() {
+        blueLabel.text = "Blue: \(round(blueSlider.value * 100) / 100)"
+        blueColor = CGFloat(blueSlider.value)
+        updateColorView()
+    }
+    
+    private func updateColorView() {
+        let color = UIColor(
+            red: redColor,
+            green: greenColor,
+            blue: blueColor,
+            alpha: 1.0
+        )
+        colorDisplayView.backgroundColor = color
+    }
 }
-
